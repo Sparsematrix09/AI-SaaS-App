382382c42e4f3612d59a6b385647bdf15563f13d
Sparsematrix09 <bt23cse041@iiitn.ac.in>
2025-08-22 20:24:34 +0530

    Update Plan.jsx

--- client/src/components/Plan.jsx
+++ client/src/components/Plan.jsx
@@ -3,16 +3,20 @@ import { PricingTable } from '@clerk/clerk-react'
 
 const Plan = () => {
   return (
-    <div className='max-w-2xl mx-auto z-20 my-30'>
+    <div className='max-w-2xl mx-auto z-20 my-30 text-white bg-gray-900 py-24'>
+      
+      {/* Section Heading */}
+      <div className='text-center'>
+        <h2 className='text-white text-[42px] font-semibold'>Choose Your Plan</h2>
+        <p className='text-gray-400 max-w-lg mx-auto'>
+          Start for free and scale up as you grow. Find the perfect plan for your content creation needs.
+        </p>
+      </div>
 
-        <div className='text-center'>
-            <h2 className='text-slate-700 text-[42px] font-semibold'>Choose Your Plan</h2>
-            <p className='text-gray-500 max-w-lg mx-auto'>Start for free and scale up as you grow. Find the perfect plan for your content creation needs.</p>
-        </div>
-
-        <div className='mt-14 max-sm:mx-8'>
-            <PricingTable/>
-        </div>
+      {/* Pricing Table */}
+      <div className='mt-14 max-sm:mx-8'>
+        <PricingTable />
+      </div>
     </div>
   )
 }
